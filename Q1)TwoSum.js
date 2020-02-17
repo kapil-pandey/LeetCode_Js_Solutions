c2c570fe1af915d5d12545ae9896b1bf62882e4a
kapil_pandey😎 <abyss6271@gmail.com>
2020-02-17 14:09:54 +0530

    Create Q1)TwoSum.js
    LeetCode 1st problem Solution

---
+++ Q1)TwoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    let required = target - nums[i]
+    if (required in map)
+      return [map[required], i]
+    map[nums[i]] = i
+  }
+}
